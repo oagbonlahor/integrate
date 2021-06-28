74df0997541ba83d86e52849ff9843174d5e9923
oagbonlahor <oagbonlahor@cloudhight.com>
2021-06-28 10:24:29 +0100

    adding IGW and NAT

--- PAMP.tf
+++ PAMP.tf
@@ -44,3 +44,24 @@ resource "aws_subnet" "Motiva_Team1_Private_SN_02" {
     Name = "Motiva_Team1_Private_SN_0.2"
   }
 }
+# Create an Internet Gateway 
+resource "aws_internet_gateway" "Motiva_Team1_IGW" {
+  vpc_id = aws_vpc.Motiva_Team1_VPC.id
+
+  tags = {
+    Name = "Motiva_Team1_IGW"
+  }
+}
+#Create a NAT Gateway
+ resource "aws_nat_gateway" "Motiva_Team1_NAT" {
+  allocation_id = aws_eip.Motiva_Team1_EIP.id
+  subnet_id     = aws_subnet.Motiva_Team1_Public_SN_01.id
+
+  tags = {
+    Name = "Motiva_Team1_NAT"
+  }
+}
+#Create EIP for Nat Gateway
+resource "aws_eip" "Motiva_Team1_EIP" {
+  vpc       = true
+}
